01545fd1220b79830cf1ed1c4139cd77249caeb5
Shruti-83 <shruti.4083@gmail.com>
2026-06-03 19:05:34 +0530

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express(); // ✅ FIRST create app
 
 // ✅ middleware
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5174'],  // allow both frontend ports
+  origin: [process.env.FRONTEND_URI, 'http://localhost:5174'],  // allow both frontend ports
   credentials: true   // keep this if you're using cookies
 }))
 app.use(express.json());
@@ -42,7 +42,7 @@ const server = http.createServer(app);
 // ✅ SOCKET.IO
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: [process.env.FRONTEND_URI, 'http://localhost:5173', 'http://localhost:5174'],
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -129,6 +129,7 @@ io.on("connection", (socket) => {
   });
 });
 
+console.log("CLIENT_URL:", process.env.FRONTEND_URI);
 // ❗ IMPORTANT: use server.listen NOT app.listen
 server.listen(process.env.PORT, () => {
   console.log("Server running on PORT " + process.env.PORT);
